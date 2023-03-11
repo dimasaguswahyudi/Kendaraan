ca6b85ca3adc484c7ff5156fe0444f174b3a67b1
DimasAgusWahyudi <dimas2.ds4@gmail.com>
2023-03-12 00:30:20 +0700

    Jwt Login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PenjualanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,10 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('me', [AuthController::class, 'me']);
 });
 
-Route::get('penjualan', [PenjualanController::class, 'index']);
-Route::get('getpenjualan/{kendaraan_id}', [PenjualanController::class, 'getPenjualan']);
-Route::post('penjualan', [PenjualanController::class, 'store']);
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('penjualan', [PenjualanController::class, 'index']);
+    Route::get('getpenjualan/{kendaraan_id}', [PenjualanController::class, 'getPenjualan']);
+    Route::post('penjualan', [PenjualanController::class, 'store']);
+});
