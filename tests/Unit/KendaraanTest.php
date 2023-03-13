f2f39f53c853b57cc15cee9b466862d5070c1b8a
DimasAgusWahyudi <dimas2.ds4@gmail.com>
2023-03-13 11:57:02 +0700

    Unit Test

---
+++ tests/Unit/KendaraanTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Mobil;
+use App\Models\Kendaraan;
+use App\Models\Motor;
+use PHPUnit\Framework\TestCase;
+
+class KendaraanTest extends TestCase
+{
+    public function creare()
+    {
+        $kendaraan1 = Kendaraan::create([
+            'tahun_keluaran' => 2017,
+            'warna' => "Biru Macho",
+            'harga' => 18727,
+        ]);
+
+        $mobil = Mobil::create([
+            'kendaraan_id' => $kendaraan1->_id,
+            'mesin' => "M62762",
+            'kapasitas_penumpang' => 6,
+            'tipe' => "T72h"
+        ]);
+
+        $kendaraan2 = Kendaraan::create([
+            'tahun_keluaran' => 2017,
+            'warna' => "Biru Macho",
+            'harga' => 18727,
+        ]);
+
+        $motor = Motor::create([
+            'kendaraan_id' => $kendaraan2->_id,
+            'mesin' => "M333",
+            'tipe_suspensi' => "T827",
+            'tipe_transmisi' => "T72h"
+        ]);
+        $this->assertTrue(true);
+    }
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
